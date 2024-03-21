cb511e7f3bde535347723f80676ddab2096ee76a
ebutejonah683@gmail.com <ebutejonah683@gmail.com>
2024-03-21 05:22:41 +0100

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0MDcwNA/spectral.js?branch_slug=master&token=6ab11a0b-c024-4507-bd15-836a2f279910";
+export default { extends: ruleset };
